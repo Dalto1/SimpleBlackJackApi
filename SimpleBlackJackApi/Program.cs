119792b6a3d61104c23f22b30188cbcbe59b82d6
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2022-10-22 12:26:41 -0400

    Mise à jour

--- SimpleBlackJackApi/Program.cs
+++ SimpleBlackJackApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -7,7 +9,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 var cardTypes = new[]
 {
     "Carreau", "Coeur", "Pique", "Trefle"
@@ -16,14 +18,12 @@ var cardValues = new[]
 {
     "Roi", "Reine", "Valet", "10", "9", "8", "7", "6", "5", "4", "3", "2", "As"
 };
-app.MapGet("/getDeck", () =>
-{
-    return 2;
-}).WithName("getDeck");
-app.MapGet("/getCard", () => new Card
+app.MapGet("/getDeck", () => new Deck("2")).WithName("getDeck");
+app.MapGet("/getCard{id}", (int deckId) => new Card
 (
     cardTypes[new Random().Next(cardTypes.Length)],
     cardValues[new Random().Next(cardValues.Length)]
 )).WithName("getCard");
 app.Run();
+internal record Deck(string id) { }
 internal record Card(string rank, string suit) { }
