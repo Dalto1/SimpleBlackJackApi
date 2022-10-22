b48ebc3d401a19440388a417c1a078b05958294c
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2022-10-22 11:24:48 -0400

    Initial

---
+++ SimpleBlackJackApi/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+var cardTypes = new[]
+{
+    "Carreau", "Coeur", "Pique", "Trefle"
+};
+var cardValues = new[]
+{
+    "Roi", "Reine", "Valet", "10", "9", "8", "7", "6", "5", "4", "3", "2", "As"
+};
+app.MapGet("/getDeck", () =>
+{
+    return 2;
+}).WithName("getDeck");
+app.MapGet("/getCard", () => new Card
+(
+    cardTypes[new Random().Next(cardTypes.Length)],
+    cardValues[new Random().Next(cardValues.Length)]
+)).WithName("getCard");
+app.Run();
+internal record Card(string rank, string suit) { }
