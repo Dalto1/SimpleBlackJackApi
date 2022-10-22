0850b6d96ac056b5ec09aa5e508264e5bb46b535
Gabriel Daoust <gabrieldaoust1@hotmail.com>
2022-10-22 15:35:33 -0400

    Fix right name

--- SimpleBlackJackApi/Program.cs
+++ SimpleBlackJackApi/Program.cs
@@ -18,12 +18,12 @@ var cardValues = new[]
 {
     "Roi", "Reine", "Valet", "10", "9", "8", "7", "6", "5", "4", "3", "2", "As"
 };
-app.MapGet("/getDeck", () => new Deck("2")).WithName("getDeck");
+app.MapGet("/getDeck", () => new Deck(2)).WithName("getDeck");
 app.MapGet("/getCard{id}", (int deckId) => new Card
 (
     cardTypes[new Random().Next(cardTypes.Length)],
     cardValues[new Random().Next(cardValues.Length)]
 )).WithName("getCard");
 app.Run();
-internal record Deck(string id) { }
+internal record Deck(int deckId) { }
 internal record Card(string rank, string suit) { }
